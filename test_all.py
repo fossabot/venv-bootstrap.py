90924fb1f0696a82ebe2a79bb081dbe29cf0fbf8
Kyrylo Shpytsya <kshpitsa@gmail.com>
2018-01-10 14:42:55 +0200

    Rework subprocess i/o in tests to unicode and EOL agnostic

--- test_all.py
+++ test_all.py
@@ -9,36 +9,16 @@ import unittest
 # rather as a smoke test.
 # Also, any bugfixes should include a dedicated test to verify the fix
 
-#####################################################################
-
-# This is a heavily stripped down version of subprocess.run to be usable with Python 3.4
-
 VERBOSE_SUBPROCESS = os.environ.get("VERBOSE_SUBPROCESS")
 
+
 class CompletedProcess(object):
+    # This is a heavily stripped down version of subprocess.CompletedProcess to be usable with Python 3.4
     def __init__(self, args, returncode, stdout=None, stderr=None):
         self.args = args
         self.returncode = returncode
         self.stdout = stdout
         self.stderr = stderr
-
-
-def subprocess_run(*popenargs, **kwargs):
-    with subprocess.Popen(*popenargs, **kwargs) as process:
-        try:
-            stdout, stderr = process.communicate()
-        except:
-            process.kill()
-            process.wait()
-            raise
-        retcode = process.poll()
-    if VERBOSE_SUBPROCESS:
-        print(popenargs[0], ' -> ', retcode)
-        print("=== stdout: ==\n", stdout.decode())
-        print("=== stderr: ==\n", stderr.decode())
-    return CompletedProcess(process.args, retcode, stdout, stderr)
-
-#####################################################################
 
 
 class FirstTestCase(unittest.TestCase):
@@ -67,38 +47,53 @@ class FirstTestCase(unittest.TestCase):
                 return i.decode()
             return str(i)
 
-        args = [convert(i) for i in args]
+        args = [sys.executable, self._script] + [convert(i) for i in args]
 
-        return subprocess_run(
-            [sys.executable, self._script] + args,
+        with subprocess.Popen(
+            args,
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        )
+            stderr=subprocess.PIPE,
+            universal_newlines=True
+        ) as process:
+            try:
+                stdout, stderr = process.communicate()
+            except:
+                process.kill()
+                process.wait()
+                raise
+            retcode = process.poll()
+
+        if VERBOSE_SUBPROCESS:
+            print(args, ' -> ', retcode)
+            print("=== stdout: ==\n", stdout)
+            print("=== stderr: ==\n", stderr)
+
+        return CompletedProcess(process.args, retcode, stdout, stderr)
 
     def test_example1_success(self):
-        MESSAGE = b'stdout message'
+        MESSAGE = 'stdout message'
         for count, count_arg in [(1, []), (1, ['--count', '1']), (2, ['--count', '2']), (0, ['--count', '0'])]:
             er = self._run_script(['venv_bootstrap_py_example1', self._example1_dir, 'succeed'] + count_arg + [MESSAGE])
             self.assertEqual(er.returncode, 0)
-            self.assertEqual(er.stdout, b''.join(count * [MESSAGE, b'\n']))
+            self.assertEqual(er.stdout, ''.join(count * [MESSAGE, '\n']))
 
     def test_example1_fail(self):
-        MESSAGE = b'stderr message'
+        MESSAGE = 'stderr message'
         CODE = 42
         er = self._run_script(['venv_bootstrap_py_example1', self._example1_dir, 'fail', '--message', MESSAGE, '--code', CODE])
         self.assertEqual(er.returncode, CODE)
-        self.assertTrue(er.stderr.endswith(MESSAGE + b'\n'))
-        self.assertEqual(er.stdout, b'')
+        self.assertTrue(er.stderr.endswith(MESSAGE + '\n'))
+        self.assertEqual(er.stdout, '')
 
     def test_example1_pip_install_error(self):
         er = self._run_script(['some_module', os.path.devnull])
         self.assertEqual(er.returncode, 2)
-        self.assertEqual(er.stdout, b'')
+        self.assertEqual(er.stdout, '')
 
     def test_example1_ImportError(self):
-        MODULE = b'some_non_existent_module'
+        MODULE = 'some_non_existent_module'
         er = self._run_script([MODULE, self._example1_dir])
         self.assertEqual(er.returncode, 2)
-        self.assertTrue(b'\nerror: ' in er.stderr)
+        self.assertTrue('\nerror: ' in er.stderr)
         self.assertTrue(MODULE in er.stderr)
-        self.assertEqual(er.stdout, b'')
+        self.assertEqual(er.stdout, '')
